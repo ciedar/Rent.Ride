eb9b4db4b3eb0b160a7fe0a2b4ccbeb03441f03d
ciedar <dciesla545@gmail.com>
2023-05-18 15:23:33 +0200

    test

--- script/views/mainView.js
+++ script/views/mainView.js
@@ -57,17 +57,17 @@ export default class View {
         })
     }
     renderMainView() {
-        const html = `<header id="header">
+        const html = `<header>
         <nav>
             <div class="logo">
                 <a href="#">Rent&Ride</a>
             </div>
             <div class="nav-links">
                 <ul>
-                    <li><a href="#header">Wypożycz</a></li>
-                    <li><a href="#section-one">Dlaczego my</a></li>
-                    <li><a href="#section-two">Flota</a></li>
-                    <li><a href="#section-three">Miasta</a></li>
+                    <li><a href="#">Wypożycz</a></li>
+                    <li><a href="#">Dlaczego my</a></li>
+                    <li><a href="#">Flota</a></li>
+                    <li><a href="#">Miasta</a></li>
                     <li><a href="#" class="btn btn-primary">Załóż konto</a></li>
                 </ul>
             </div>
@@ -78,20 +78,16 @@ export default class View {
             <div class="search-form">
                 <form>
                     <div class="form-group">
-                    <label for="location">Skąd?</label>
-                    <select id="location" name="location">
-                      <option value="">Wybierz miejsce</option>
-                      <option value="Poznań">Poznań</option>
-                      <option value="Gdańsk">Gdańsk</option>
-                      <option value="Warszawa">Warszawa</option>
-                      <option value="Kraków">Kraków</option>
-                    </select>
+                        <label for="location">Skąd?</label>
+                        <input type="text" id="location" name="location" placeholder="Ville, aéroport, gare...">
                     </div>
                     <div class="form-group-dates">
                         <label for="dates">Data odbioru</label>
-                        <input class="dateIn" type="date" id="dateIn" name="dateIn">
-                        <label for="passengers">Data zwrotu</label>
-                        <input class="dateOut" type="date" id="dateOut" name="dateOut">
+                        <input class="dateIn" type="date" id="dateIn" name="dates"
+                            placeholder="Date et heure de départ et de retour">
+                        <label for="passengers">Ilość pasażerów</label>
+                        <input class="dateOut" type="date" id="dateOut" name="passengers"
+                            placeholder="Nombre de passagers">
                     </div>
                     <div class="form-group">
                         <button type="submit" class="btn btn-secondary">Wyszukaj!</button>
@@ -100,6 +96,7 @@ export default class View {
             </div>
         </div>
     </header>
+
     <section class="about-us section" id="section-one">
         <div class="about-us-container">
             <div class="div-title">
@@ -167,18 +164,108 @@ export default class View {
             </div>
     </section>
     <section class="section our-location" id="section-three">
-        <div class="figure-div">
-            <div class="div-fig div-info-one">
-                <h2>Gdańsk</h2>
+        <div class="card card-gda">
+            <div class="card-inner">
+                <div class="card-side card-side-front">
+                    <div class="card-image">
+                        <img src="/images/cities/gdansk.jpg" alt="card-img">
+                    </div>
+                    <div class="card-body">
+                        <h2 class="card-title">Gdańsk</h2>
+                        <span class="card-arrow">&rarr;</span>
+                    </div>
+                </div>
+                <div class="card-side card-side-back">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A laboriosam dolores pariatur
+                            placeat quaerat est, unde, officiis harum facere aliquid eaque iste rem consequuntur quas
+                            quibusdam rerum animi? Eius blanditiis dolorum numquam vel, obcaecati odit! Optio deleniti
+                            recusandae, iusto quaerat rem est totam incidunt obcaecati voluptas vitae voluptate
+                            veritatis, nihil atque unde modi officiis nobis! Atque sapiente, esse beatae dolor ducimus
+                            aspernatur, id tenetur illo eveniet quos omnis dicta! Nam aliquid laudantium voluptatem
+                            possimus. Distinctio quibusdam quis officiis explicabo ipsa quisquam aut vitae quas commodi
+                            laboriosam fugit deserunt velit labore ullam, voluptatibus dicta porro eos laborum, error
+                            ratione! Consequatur, voluptatem.</p>
+                    </div>
+                </div>
             </div>
-            <div class="div-fig div-info-two">
-                <h2>Poznań</h2>
+        </div>
+        <div class="card card-poz">
+            <div class="card-inner">
+                <div class="card-side card-side-front">
+                    <div class="card-image">
+                        <img src="/images/cities/poznan.jpg" alt="card-img">
+                    </div>
+                    <div class="card-body">
+                        <h2 class="card-title">Gdańsk</h2>
+                        <span class="card-arrow">&rarr;</span>
+                    </div>
+                </div>
+                <div class="card-side card-side-back">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga repellat illum autem
+                            dignissimos vero minus accusamus, quam necessitatibus quod iusto. Reiciendis tempore eius
+                            qui inventore officia esse numquam provident iste molestiae nulla consectetur itaque harum
+                            nisi illum, minus labore ea perspiciatis, enim neque veritatis? Quia ad deleniti laborum
+                            amet beatae autem minima impedit, accusamus natus blanditiis! Fugit, excepturi! Facere
+                            aliquid corrupti adipisci ipsum at vero dolore nam animi. Impedit unde qui delectus
+                            excepturi nostrum reiciendis praesentium, quis veniam accusamus, consequuntur, perspiciatis
+                            quia cupiditate? Facere tempore, labore accusamus tenetur similique quo, veritatis deserunt
+                            dignissimos rem numquam pariatur quae, eos error. Quae!</p>
+                    </div>
+                </div>
             </div>
-            <div class="div-fig div-info-three">
-                <h2>Warszawa</h2>
+        </div>
+        <div class="card card-war">
+            <div class="card-inner">
+                <div class="card-side card-side-front">
+                    <div class="card-image">
+                        <img src="/images/cities/warsaw.jpg" alt="card-img">
+                    </div>
+                    <div class="card-body">
+                        <h2 class="card-title">Gdańsk</h2>
+                        <span class="card-arrow">&rarr;</span>
+                    </div>
+                </div>
+                <div class="card-side card-side-back">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga tempora nostrum harum
+                            recusandae odit sapiente id fugit, ad quasi veritatis quidem, animi itaque maxime numquam
+                            eligendi soluta quis minima vero sit excepturi labore consectetur asperiores ex reiciendis?
+                            Dicta aperiam soluta sunt sit expedita quia sequi veniam adipisci impedit nihil distinctio
+                            suscipit, doloremque fugit numquam eos? Eveniet magnam facere libero molestiae sunt quis
+                            dicta earum ipsa amet natus praesentium deleniti ut nisi nemo quos architecto accusamus
+                            dolore laboriosam, tenetur itaque nam nihil numquam eos quod? Fugit laudantium ullam
+                            obcaecati nobis sunt adipisci, iste explicabo placeat quo voluptatibus necessitatibus
+                            provident, vero in.</p>
+                    </div>
+                </div>
             </div>
-            <div class="div-fig div-info-four">
-                <h2>Kraków</h2>
+        </div>
+        <div class="card card-kra">
+            <div class="card-inner">
+                <div class="card-side card-side-front">
+                    <div class="card-image">
+                        <img src="/images/cities/krakow.jpg" alt="card-img">
+                    </div>
+                    <div class="card-body">
+                        <h2 class="card-title">Gdańsk</h2>
+                        <span class="card-arrow">&rarr;</span>
+                    </div>
+                </div>
+                <div class="card-side card-side-back">
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ratione unde corrupti,
+                            expedita debitis tenetur eos optio amet dolores ipsam odit alias cumque eius impedit
+                            voluptate reprehenderit aspernatur. Recusandae aliquid molestias corrupti animi quis
+                            officiis optio dolore esse ullam perferendis? Aut odio adipisci architecto quia aliquam
+                            quidem ipsam alias. Quo rem quae est saepe dolores perferendis temporibus consequuntur
+                            aspernatur quis. Excepturi aut dolorum maxime sint nesciunt ab eaque, omnis velit quidem
+                            facere deleniti itaque quam ut numquam atque possimus fugiat sunt fugit eveniet blanditiis
+                            provident! Eum odio voluptate nihil odit repellendus, qui doloremque esse vero neque aliquid
+                            soluta enim culpa!</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
@@ -187,7 +274,7 @@ export default class View {
             <h4>Dariusz Cieśla</h4>
         </div>
 
-                    </footer>`;
+    </footer>`;
         this.clear();
         this.parentContainer.insertAdjacentHTML("afterbegin", html);
     }
